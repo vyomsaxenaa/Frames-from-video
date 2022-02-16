61ab87cfabf81135bc4a64bbb1da427f8a87b6d4
vyomsaxenaa <98803245+vyomsaxenaa@users.noreply.github.com>
2022-02-16 12:48:39 +0530

    Update Video frames.py

--- Video frames.py
+++ Video frames.py
@@ -1,17 +1,21 @@
 # Problem 1: No import packages
-
+import cv2
+#To read the Video
 cap=cv2.VideoCapture("sid concert.mpg")
-ret, frame=cap.read() # Problem 2: Dont need to read the video here
-while(1): # why use while(1): trying to use boolean True as one?
+ret, frame=cap.read()
+count=0
+while(True):
+    #To read the frames
     ret, frame=cap.read()
-    cv2.imshow("frame",frame) # We dont need this. System will hang. Too many open windows.
-    #Comment for each line; what and why you are doing.
-    fps=cap.get(cv2.CAP_PROP_FPS)
-    frame_count = (int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
-    duration=frame_count/fps
-    cv2.imwrite("Frames.jpg",duration)
+    print("read a new frame:",frame)
+    # To capture frame at specific time
+    if(count%30==0):
+        cv2.imwrite("frame%d.jpg"%count,frame)
+    count+=1
+    # To stop the Iteration 
     if(cv2.waitKey(1)& 0xFF==ord("q") or ret==False):
         cap.release()
         cv2.destroyAllWindows()
         break
     cv2.imshow("frame",frame)
+
