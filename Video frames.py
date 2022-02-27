298ab534af82ca673f7ac27f0b55f25b36d66921
vyomsaxenaa <98803245+vyomsaxenaa@users.noreply.github.com>
2022-02-27 13:02:36 +0530

    Update Video frames.py

--- Video frames.py
+++ Video frames.py
@@ -17,4 +17,18 @@ while(True):
         cv2.destroyAllWindows()
         break
     cv2.imshow("frame",frame)
+    
+    
+#Face Detection
+import cv2
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+#read the image
+img=cv2.imread("friends.jpg")
+gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces=face_cascade.detectMultiScale(gray,1.1,3)
 
+for(x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y),(x+w, y+h), (0,255,0), 3)
+
+cv2.imshow("image",img)
+cv2.waitkey()
