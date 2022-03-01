2ddb0e020dc83a2dc02dc7a8fc5a3c95ecd98c4b
vyomsaxenaa <98803245+vyomsaxenaa@users.noreply.github.com>
2022-03-01 13:31:36 +0530

    Update Video frames.py

--- Video frames.py
+++ Video frames.py
@@ -1,10 +1,20 @@
 import cv2
-cap=cv2.VideoCapture("sid concert.mpg")
+path="sid concert.mpg"
+cap=cv2.VideoCapture(path) 
+ret, frame=cap.read()
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 count=0
 while(True):
     #To read the frames
     ret, frame=cap.read()
     print("read a new frame:",frame)
+    img=cap.read()
+    """ Face Detectiion"""
+    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(gray,1.1,3)
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y),(x+w, y+h), (0,255,0), 3)
     # To capture frame at specific time
     fps=cap.get(cv2.CAP_PROP_FPS)
     time=fps*5
@@ -16,19 +26,5 @@ while(True):
         cap.release()
         cv2.destroyAllWindows()
         break
-    cv2.imshow("frame",frame)
-    
-    
-#Face Detection
-import cv2
-face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-#read the image
-img=cv2.imread("friends.jpg")
-gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces=face_cascade.detectMultiScale(gray,1.1,3)
-
-for(x,y,w,h) in faces:
-    cv2.rectangle(img, (x,y),(x+w, y+h), (0,255,0), 3)
-
-cv2.imshow("image",img)
-cv2.waitkey()
+    cv2.imshow("Frame",frame)
+cap.release()
