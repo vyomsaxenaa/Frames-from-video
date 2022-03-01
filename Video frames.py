a412e7af77f0d234fbfad40e738300ef42191c65
vyomsaxenaa <98803245+vyomsaxenaa@users.noreply.github.com>
2022-03-01 19:32:27 +0530

    Update Video frames.py

--- Video frames.py
+++ Video frames.py
@@ -14,11 +14,12 @@ while(True):
     faces=face_cascade.detectMultiScale(gray,1.1,3)
 
     for(x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y),(x+w, y+h), (0,255,0), 3)
+        cv2.rectangle(frame, (x,y),(x+w, y+h), (255,0,0), 3)
     # To capture frame at specific time
     fps=cap.get(cv2.CAP_PROP_FPS)
     time=fps*5
-    if(count%time==0):
+    #if(count%time==0):
+    if(faces):
         cv2.imwrite("frame%d.jpg"%count,frame)
     count+=1
     # To stop the Iteration 
