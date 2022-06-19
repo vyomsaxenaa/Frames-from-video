ec4fca287b9cc85b818cf26ccb317b386748e010
vyomsaxenaa <98803245+vyomsaxenaa@users.noreply.github.com>
2022-06-19 18:57:10 +0530

    Update All_frames_impli.py

--- All_frames_impli.py
+++ All_frames_impli.py
@@ -1,12 +1,7 @@
 import cv2
 import os
 import frames_func as fn
-#path="C:/Users/Sid/Desktop/Vyom's Python/Video Sample/"
-#for file in os.listdir(path):
- #   if file.endswith(".mp4"):
-  #      loc=os.path.join(path,file)
-   #     video_read(loc)
-#frame=fn.video_read("sid concert.mpg")
+
 frame=fn.video_read(file)
 cv2.imshow("Frame",frame)
 print(frame)
